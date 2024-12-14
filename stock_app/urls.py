daa0bf81cd1d5ae160d6ad89feba8f292b6f48b3
Sachin <sachnaror@gmail.com>
2024-12-15 03:21:05 +0530

    Daily learning, lifetime achievement

--- stock_app/urls.py
+++ stock_app/urls.py
@@ -1,1 +1,7 @@
-# Your urls configuration here
+from django.urls import path
+from .views import get_stock_data, get_news_articles
+
+urlpatterns = [
+    path("stock/<str:ticker>/", get_stock_data, name="stock_data"),
+    path("news/<str:ticker>/", get_news_articles, name="news_articles"),
+]
--- stock_app/views.py
+++ stock_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from stock_app.models import StockData
+from pymongo import MongoClient
 
-# Create your views here.
+client = MongoClient("mongodb://localhost:27017/")
+news_db = client["finance_data"]
+
+@api_view(["GET"])
+def get_stock_data(request, ticker):
+    stock = StockData.objects.filter(ticker=ticker).values()
+    return Response({"data": list(stock)})
+
+@api_view(["GET"])
+def get_news_articles(request, ticker):
+    news = news_db.news_articles.find({"ticker": ticker})
+    return Response([{"title": item["title"], "link": item["link"]} for item in news])
